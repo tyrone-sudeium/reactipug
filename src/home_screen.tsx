47654306ded8b53a3d318a3151e72044be8cc818
Tyrone Trevorrow <tyrone@sudeium.com>
2017-08-26 22:49:05 +1000

    style(home): JSX cleanup

--- src/home_screen.tsx
+++ src/home_screen.tsx
@@ -74,28 +74,26 @@ export class HomeScreen extends React.Component<NavigationScreenProps<{}>, HomeS
   render(): JSX.Element {
     return (
       <Container>
-        <Content padder style={{padding: 64}}>
-          <View style={{flexDirection: "column", alignItems: "stretch"}}>
-            <View style={{padding: 20, alignItems: "center"}}>
-              <Image source={require("./img/pug.png")}/>
-            </View>
-            <View style={{backgroundColor: "white", paddingBottom: 8}}>
-              <Text style={{fontWeight: "bold", padding: 8}}>Pug pages</Text>
-              <PugPageSlider pages={this.state.pugPages} onValueChange={(newVal) => this.setPugPages(newVal)}/>
-            </View>
-            <View style={{marginTop: 8, flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
-              <Button iconLeft success 
-                style={{margin: 4}} 
-                onPress={() => this.showMePuppers()}
-              >
-                <Icon active name="paw"/>
-                <Text style={{color: "white"}}>Show me the puppers</Text>
-              </Button>
-              <Button iconLeft info style={{margin: 4}}>
-                <Icon active name="alert"/>
-                <Text style={{color: "white"}}>About this silly app</Text>
-              </Button>
-            </View>
+        <Content padder>
+          <View style={{padding: 20, alignItems: "center"}}>
+            <Image source={require("./img/pug.png")}/>
+          </View>
+          <View style={{backgroundColor: "white", paddingBottom: 8}}>
+            <Text style={{fontWeight: "bold", paddingHorizontal: 8, paddingVertical: 8}}>Pug pages</Text>
+            <PugPageSlider pages={this.state.pugPages} onValueChange={(newVal) => this.setPugPages(newVal)}/>
+          </View>
+          <View style={{marginTop: 8, flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
+            <Button iconLeft success 
+              style={{margin: 4}} 
+              onPress={() => this.showMePuppers()}
+            >
+              <Icon active name="paw"/>
+              <Text style={{color: "white"}}>Show me the puppers</Text>
+            </Button>
+            <Button iconLeft info style={{margin: 4}}>
+              <Icon active name="alert"/>
+              <Text style={{color: "white"}}>About this silly app</Text>
+            </Button>
           </View>
         </Content>
       </Container>
